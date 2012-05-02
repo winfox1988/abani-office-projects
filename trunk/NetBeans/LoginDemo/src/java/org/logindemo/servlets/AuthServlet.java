6e152102b4dd6d584b2dfb0f561b0403d4f265b4
abani.abani@gmail.com <abani.abani@gmail.com@a1c241a0-50a5-ba6d-4cc5-f3e7b8b01aed>
2012-05-02 09:15:22 +0000

    LoginDemo with Tomcat7

---
+++ trunk/NetBeans/LoginDemo/src/java/org/logindemo/servlets/AuthServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.logindemo.servlets;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author behera
+ */
+@WebServlet(name = "AuthServlet", urlPatterns = {"/login", "/logout"})
+public class AuthServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if ( request.getServletPath().equalsIgnoreCase("/login") ) {
+            processLogin(request, response);
+        }
+        else {
+            request.logout();
+            request.setAttribute("msg", "You have successfully logged out of system");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+    }
+
+    
+    private void processLogin(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("uid");
+        String password = request.getParameter("pwd");
+        RequestDispatcher rd = null;
+        try {
+            request.login(username, password);
+            rd = request.getRequestDispatcher("myaccount.jsp");
+           
+        } catch(Exception e){
+            request.setAttribute("msg", "Unable to login with <b> " + username + "</b>");
+            rd = request.getRequestDispatcher("login.jsp");
+        }
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
